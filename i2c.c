28e6581dd00b3eecaeec35e18a01267fc838e4d4
jaewoos98 <wodn0618@gmail.com>
2024-07-02 09:26:57 +0900

    Update i2c.c

--- i2c.c
+++ i2c.c
@@ -5,12 +5,12 @@
 //YL-40 가변 저항
 main()
 {
-	int fd = wiringPiI2CSetup(0x48);		//dh = device handle
-	wiringPiI2CWrite(fd, 3);				//channel 03 (VR) = 가변 저항(Variable Resistor) / 다른 채널번호 사용하면 온도,조도센서 사용가능
+	int dh = wiringPiI2CSetup(0x48);		//dh = device handle
+	wiringPiI2CWrite(dh, 3);				//channel 03 (VR) = 가변 저항(Variable Resistor) / 다른 채널번호 사용하면 온도,조도센서 사용가능
 	
 	while(1)
 	{
-		int val = wiringPiI2CRead(fd);
+		int val = wiringPiI2CRead(dh);
 		float f = (float)((val/255.0)*5.0);
 		printf("VR input level = %d (%4.1fV)\n" , val, f);
 		delay(500);
